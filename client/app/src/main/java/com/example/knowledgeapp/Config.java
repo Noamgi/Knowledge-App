76e54804216bb5e408a0911c088365d4b03a3a49
noam <noamginsberg@gmail.com>
2020-04-27 23:53:53 +0300

    add the YouTube API

---
+++ client/app/src/main/java/com/example/knowledgeapp/Config.java
@@ -0,0 +1,10 @@
+package com.example.knowledgeapp;//package com.example.knowledgeapp;
+
+public final class Config {
+
+    private Config() {
+    }
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyD9aVbM1gUwd7uaHUp4uTPFoMzzOSAUqyc";
+
+}
