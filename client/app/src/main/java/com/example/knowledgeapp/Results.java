6982df2c3f21586a9bc70e0d1cdfae3b80807d89
noam <noamginsberg@gmail.com>
2020-05-24 12:35:28 +0300

    change video format

--- client/app/src/main/java/com/example/knowledgeapp/Results.java
+++ client/app/src/main/java/com/example/knowledgeapp/Results.java
@@ -2,43 +2,204 @@ package com.example.knowledgeapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.youtube.player.YouTubeBaseActivity;
 import com.google.android.youtube.player.YouTubeInitializationResult;
 import com.google.android.youtube.player.YouTubePlayer;
+import com.google.android.youtube.player.YouTubePlayer.PlaybackEventListener;
 import com.google.android.youtube.player.YouTubePlayer.Provider;
 import com.google.android.youtube.player.YouTubePlayerView;
+import com.google.android.youtube.player.YouTubePlayer.PlayerStyle;
+import static com.google.android.youtube.player.YouTubePlayer.ErrorReason;
+import static com.google.android.youtube.player.YouTubePlayer.OnInitializedListener;
+import static com.google.android.youtube.player.YouTubePlayer.PlayerStateChangeListener;
 
-public class Results extends YouTubeBaseActivity implements YouTubePlayer.OnInitializedListener {
+public class Results extends YouTubeBaseActivity implements OnInitializedListener, View.OnClickListener {
 
     private static final int RECOVERY_REQUEST = 1;
     private YouTubePlayerView youTubeView;
+
+    //https://www.youtube.com/watch?v=<VIDEO_ID>
+    public static final String VIDEO_ID = "eIho2S0ZahI";
+    private YouTubePlayer mPlayer;
+    private View mPlayButtonLayout;
+    private TextView mPlayTimeTextView;
+
+    private Handler mHandler = null;
+    private SeekBar mSeekBar;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
 
-    youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_view);
-    youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+        // Initializing YouTube player view
+        youTubeView = (YouTubePlayerView) findViewById(R.id.youtube_player_view);
+        youTubeView.initialize(Config.YOUTUBE_API_KEY, this);
+
+
+
+        //Add play button to explicitly play video in YouTubePlayerView
+        mPlayButtonLayout = findViewById(R.id.video_control);
+        findViewById(R.id.play_video).setOnClickListener(this);
+        findViewById(R.id.pause_video).setOnClickListener(this);
+
+        mPlayTimeTextView = (TextView) findViewById(R.id.play_time);
+        mSeekBar = (SeekBar) findViewById(R.id.video_seekbar);
+        mSeekBar.setOnSeekBarChangeListener(mVideoSeekBarChangeListener);
+
+        mHandler = new Handler();
     }
 
     @Override
     public void onInitializationSuccess(Provider provider, YouTubePlayer player, boolean wasRestored) {
+        if (null == player) return;
+        mPlayer = player;
+
+        displayCurrentTime();
+
+
         if (!wasRestored) {
-            player.cueVideo("fhWaJi1Hsfo"); // Plays https://www.youtube.com/watch?v=fhWaJi1Hsfo
+            player.cueVideo("eIho2S0ZahI"); // Plays https://www.youtube.com/watch?v=eIho2S0ZahI
         }
+
+        player.setPlayerStyle(PlayerStyle.CHROMELESS);
+        mPlayButtonLayout.setVisibility(View.VISIBLE);
+
+        // Add listeners to YouTubePlayer instance
+        player.setPlayerStateChangeListener(mPlayerStateChangeListener);
+        player.setPlaybackEventListener(mPlaybackEventListener);
     }
 
     @Override
     public void onInitializationFailure(Provider provider, YouTubeInitializationResult errorReason) {
+
+//        Toast.makeText(this, "Failed to initialize.", Toast.LENGTH_LONG).show();
+
         if (errorReason.isUserRecoverableError()) {
             errorReason.getErrorDialog(this, RECOVERY_REQUEST).show();
         } else {
             String error = String.format(getString(R.string.player_error), errorReason.toString());
             Toast.makeText(this, error, Toast.LENGTH_LONG).show();
         }
+    }
+
+    PlaybackEventListener mPlaybackEventListener = new PlaybackEventListener() {
+        @Override
+        public void onBuffering(boolean arg0) {
+        }
+
+        @Override
+        public void onPaused() {
+            mHandler.removeCallbacks(runnable);
+        }
+
+        @Override
+        public void onPlaying() {
+            mHandler.postDelayed(runnable, 100);
+            displayCurrentTime();
+        }
+
+        @Override
+        public void onSeekTo(int arg0) {
+            mHandler.postDelayed(runnable, 100);
+        }
+
+        @Override
+        public void onStopped() {
+            mHandler.removeCallbacks(runnable);
+        }
+    };
+
+    PlayerStateChangeListener mPlayerStateChangeListener = new PlayerStateChangeListener() {
+        @Override
+        public void onAdStarted() {
+        }
+
+        @Override
+        public void onError(ErrorReason arg0) {
+        }
+
+        @Override
+        public void onLoaded(String arg0) {
+        }
+
+        @Override
+        public void onLoading() {
+        }
+
+        @Override
+        public void onVideoEnded() {
+        }
+
+        @Override
+        public void onVideoStarted() {
+            displayCurrentTime();
+        }
+    };
+
+    SeekBar.OnSeekBarChangeListener mVideoSeekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
+        @Override
+        public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+            long lengthPlayed = (mPlayer.getDurationMillis() * progress) / 100;
+            mPlayer.seekToMillis((int) lengthPlayed);
+        }
+
+        @Override
+        public void onStartTrackingTouch(SeekBar seekBar) {
+
+        }
+
+        @Override
+        public void onStopTrackingTouch(SeekBar seekBar) {
+
+        }
+    };
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play_video:
+                if (null != mPlayer && !mPlayer.isPlaying())
+                    mPlayer.play();
+                break;
+            case R.id.pause_video:
+                if (null != mPlayer && mPlayer.isPlaying())
+                    mPlayer.pause();
+                break;
+        }
+    }
+
+    private String formatTime(int millis) {
+        int seconds = millis / 1000;
+        int minutes = seconds / 60;
+        int hours = minutes / 60;
+
+        return (hours == 0 ? "--:" : hours + ":") + String.format("%02d:%02d", minutes % 60, seconds % 60);
+    }
+
+    private Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            displayCurrentTime();
+            mHandler.postDelayed(this, 100);
+        }
+    };
+
+
+
+    private void displayCurrentTime() {
+        if (null == mPlayer) return;
+        String formattedTime = formatTime(mPlayer.getDurationMillis() - mPlayer.getCurrentTimeMillis());
+        mPlayTimeTextView.setText(formattedTime);
     }
 
     @Override
@@ -51,5 +212,10 @@ public class Results extends YouTubeBaseActivity implements YouTubePlayer.OnInit
 
     protected Provider getYouTubePlayerProvider() {
         return youTubeView;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 }
