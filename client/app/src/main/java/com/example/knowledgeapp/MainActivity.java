4fe5b05adb39ead1ec90995b7c30a5287e654467
noam <noamginsberg@gmail.com>
2020-06-09 19:54:15 +0300

    add more Ui button

--- client/app/src/main/java/com/example/knowledgeapp/MainActivity.java
+++ client/app/src/main/java/com/example/knowledgeapp/MainActivity.java
@@ -1,19 +1,21 @@
 package com.example.knowledgeapp;
 
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import org.json.JSONException;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
     private Button button;
+    private Button topicButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = (Button) findViewById(R.id.button);
+        topicButton = (Button) findViewById(R.id.topic6);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -28,7 +32,24 @@ public class MainActivity extends AppCompatActivity {
                 //openResultsActivity();
             }
         });
+
+        topicButton.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+//
+//                topicButton.setBackgroundColor(getResources().getColor(R.color.colorAccent));
+//                topicButton.setTextColor(getResources().getColor(R.color.white));
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN) {
+                    topicButton.setBackground(getResources().getDrawable(R.drawable.clickedtopics));
+                    topicButton.setTextColor(Color.parseColor("#ffffffff"));
+                   // topicButton.setTextColor(Color.parseColor("ffffffff"));
+
+                }
+            }
+        });
     }
+
 
     public void openResultsActivity(String id) {
         Intent intent = new Intent(this, Results.class);
