27c004df48d53f3cf3da2d34d03e777c96ef29e9
Noam Ginsberg <57439417+Noamgi@users.noreply.github.com>
2020-06-09 20:10:46 +0300

    Update Config.java

--- client/app/src/main/java/com/example/knowledgeapp/Config.java
+++ client/app/src/main/java/com/example/knowledgeapp/Config.java
@@ -5,6 +5,6 @@ public final class Config {
     private Config() {
     }
 
-    public static final String YOUTUBE_API_KEY = "AIzaSyCfMX5ANqL5a9WjTqk21UcfzWUbtCuM7QU";
+    public static final String YOUTUBE_API_KEY = "";
 
 }
